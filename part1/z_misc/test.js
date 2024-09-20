9c00974d4dcbeb0466c4716e6120bcbdb2fe89f5
manrubbra <manrubbra@protonmail.com>
2024-09-20 13:21:20 +0200

    ADD - Miscelanious

---
+++ part1/z_misc/test.js
@@ -0,0 +1,5 @@
+console.log('Hello')
+
+// var   -> Se define de manera local. Se pueden volver a declarar.
+// let   -> Se define de manera "regional" entre {}. No se pueden volver a declarar.
+// const -> Se define de manera "regional". Solo se instancia una vez y no se puede modificar
