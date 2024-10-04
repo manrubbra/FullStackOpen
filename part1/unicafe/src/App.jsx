4d3aca0452f5d9e5873755cdef09e81361652eba
manrubbra <manrubbra@protonmail.com>
2024-10-04 13:22:25 +0200

    ADD - Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -28,16 +28,64 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  var updateGood;
+  var updateNeutral;
+  var updateBad;
+  var updateTotal;
+  var updatePositive;
+
+  var auxAverage = 0;
+
+  const handleGood = () => {
+    updateGood = good + 1;
+    updateTotal = total + 1;
+    updatePositive = updateGood / updateTotal;
+    setGood(updateGood);
+    setTotal(updateTotal);
+    auxAverage++;
+    setAverage(auxAverage / updateTotal);
+    setPositive(updatePositive);
+  };
+
+  const handleNeutral = () => {
+    updateNeutral = neutral + 1;
+    updateTotal = total + 1;
+    updatePositive = good / updateTotal;
+    setNeutral(updateNeutral);
+    setTotal(updateTotal);
+    setAverage(auxAverage / updateTotal);
+    setPositive(updatePositive);
+  };
+
+  const handleBad = () => {
+    updateBad = bad + 1;
+    updateTotal = total + 1;
+    updatePositive = good / updateTotal;
+    setBad(updateBad);
+    setTotal(updateTotal);
+    auxAverage--;
+    setAverage(auxAverage / updateTotal);
+    setPositive(updatePositive);
+  };
+
   return (
     <div>
       <Title title="Give feedback" />
-      <Button text="Good" onClick={() => setGood(good + 1)} />
-      <Button text="Neutral" onClick={() => setNeutral(neutral + 1)} />
-      <Button text="Bad" onClick={() => setBad(bad + 1)} />
+      <Button text="Good" onClick={handleGood} />
+      <Button text="Neutral" onClick={handleNeutral} />
+      <Button text="Bad" onClick={handleBad} />
       <Title title="Statistic" />
       <Display text="Good" value={good} />
       <Display text="Neutral" value={neutral} />
       <Display text="Bad" value={bad} />
+      <hr></hr>
+      <Display text="Total" value={total} />
+      <Display text="Average" value={average} />
+      <Display text="Positive" value={positive} />
     </div>
   );
 };
