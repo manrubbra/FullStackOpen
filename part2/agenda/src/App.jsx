a667a4b24cc710881eab61859523bd8aefc9568d
manrubbra <manrubbra@protonmail.com>
2024-10-09 14:23:10 +0200

    ADD - Exercise 2.6

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -1,9 +1,44 @@
 import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: 1 }]);
+  const [newName, setNewName] = useState('');
 
-  return <h1>AGENDA</h1>;
-}
+  const onChangeInput = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const onSubmitButton = (event) => {
+    var updateAgenda = persons.concat({
+      name: newName,
+      id: persons.length + 1
+    });
+    setPersons(updateAgenda);
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={onChangeInput} />
+        </div>
+        <div>
+          <button type="button" onClick={onSubmitButton}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((p) => (
+          <li key={p.id}>{p.name}</li>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
