2fdabfc36c4f4b623de223557f548155a04d2a33
manrubbra <manrubbra@protonmail.com>
2024-10-18 09:17:58 +0200

    ADD - First step with nodejs and Express

--- part3/example/index.js
+++ part3/example/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 let notes = [
   {
     id: 1,
@@ -25,6 +32,49 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    return note.id === id;
+  });
+  console.log(note);
+  // When you check the object is always truthy, just in case of undefined
+  // is falsy
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId()
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
