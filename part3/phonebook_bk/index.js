5b6aabf041eb494b2e5f4f06f71a3abe39f3503d
manrubbra <manrubbra@protonmail.com>
2024-10-21 14:12:21 +0200

    ADD - Exercise 3.4

--- part3/phonebook_bk/index.js
+++ part3/phonebook_bk/index.js
@@ -24,33 +24,53 @@ let contacts = [
   }
 ];
 
+/** Get all contacts from Contacts */
 app.get('/api/persons', (request, response) => {
   console.log(request);
   response.json(contacts);
 });
 
+//** Get the contact with the same id */
 app.get('/api/persons/:id', (request, response) => {
   var id = request.params.id;
   console.log('#DEBUG -> Id: ', id);
 
   var contact = contacts.find((c) => c.id == id);
 
-  if (contact) {
-    response.json(contact);
-  } else {
-    console.log('Contact not found!');
-    response.status(404).end();
-  }
+  if (contact) response.json(contact);
+
+  console.log('Contact not found!');
+  response.status(404).end();
 });
 
+//** Get the status (number of contacts) at this moment */
 app.get('/info', (request, response) => {
   console.log('#DEBUG -> Call to info');
 
+  // Create the html base
   let htmlResponse = `<p>Phonebook has info for ${contacts.length} people</p>`;
   htmlResponse += '<br/>';
   htmlResponse += `<p>${new Date()}</p>`;
 
   response.send(htmlResponse);
+});
+
+//** Delete the contact with the specific Id */
+app.delete('/api/persons/:id', (request, response) => {
+  var id = request.params.id;
+  console.log('#DEBUG -> Delete contact with id:');
+
+  var contact = contacts.find((c) => c.id == id);
+
+  if (!contact) {
+    console.log('Contact not found!');
+    response.status(404).end();
+  }
+
+  contacts = contacts.filter((c) => c.id != id);
+  console.log('#DEBUG -> Contacts list filtered');
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
