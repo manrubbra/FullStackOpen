497c297724d7e1d814a8a6df80e5f2ce7ab920b6
manrubbra <manrubbra@protonmail.com>
2024-10-31 13:40:10 +0100

    ADD - Move the port to env file

--- part3/phonebook_bk/index.js
+++ part3/phonebook_bk/index.js
@@ -145,7 +145,7 @@ app.post('/api/persons', (request, response) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
